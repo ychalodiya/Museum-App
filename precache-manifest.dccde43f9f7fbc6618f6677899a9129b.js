7570a81e3949724c98a26786be6e9140b44ca5bf
Yogesh Chalodiya <ychalodiya@gmail.com>
2019-04-27 14:39:42 -0400

    Updates

---
+++ precache-manifest.dccde43f9f7fbc6618f6677899a9129b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfa651c02978afc9109426d3406c044",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "ba0c4f4b74881b83fec6",
+    "url": "/museum-app/static/css/main.b5231b2a.chunk.css"
+  },
+  {
+    "revision": "6c2d77bdd636ad28fb95",
+    "url": "/museum-app/static/js/2.f15c971f.chunk.js"
+  },
+  {
+    "revision": "ba0c4f4b74881b83fec6",
+    "url": "/museum-app/static/js/main.96462034.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
